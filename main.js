e1817880399a97619fb1e1a832f534648d48ec2c
vineethaflarence <75540202+vineethaflarence@users.noreply.github.com>
2024-02-29 23:39:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,217 @@
+function what_to_bring_card(){
+    document.getElementById("getting-started").style.display="none";
+    document.getElementById("what-to-bring").style.display="block";
+    document.getElementById("Institute-account").style.display="none";
+    document.getElementById("get-online").style.display="none";
+    document.getElementById("getting-on-email").style.display="none";
+    document.getElementById("get-help").style.display="none";
+    document.getElementById("free-help").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+}
+
+function Institute_account_card(){
+    document.getElementById("getting-started").style.display="none";
+    document.getElementById("what-to-bring").style.display="none";
+    document.getElementById("Institute-account").style.visibility='visible';
+    document.getElementById("get-online").style.display="none";
+    document.getElementById("getting-on-email").style.display="none";
+    document.getElementById("get-help").style.display="none";
+    document.getElementById("free-help").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+  
+}
+
+function getting_online_card(){
+    document.getElementById("getting-started").style.display="none";
+    document.getElementById("what-to-bring").style.display="none";
+    document.getElementById("Institute-account").style.display="none";
+    document.getElementById("get-online").style.display="block";
+    document.getElementById("getting-on-email").style.display="none";
+    document.getElementById("get-help").style.display="none";
+    document.getElementById("free-help").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+
+}
+function getting_onto_email_card(){
+    document.getElementById("getting-started").style.display="none";
+    document.getElementById("what-to-bring").style.display="none";
+    document.getElementById("Institute-account").style.display="none";
+    document.getElementById("get-online").style.display="none";
+    document.getElementById("getting-on-email").style.display="block";
+    document.getElementById("get-help").style.display="none";
+    document.getElementById("free-help").style.display="none";
+
+    document.getElementById("mail-client").style.display="none";
+}
+function getting_help_card(){
+    document.getElementById("getting-started").style.display="none";
+
+    document.getElementById("Institute-account").style.display="none";
+    document.getElementById("get-online").style.display="none";
+    document.getElementById("getting-on-email").style.display="none";
+    document.getElementById("get-help").style.display="block";
+    document.getElementById("free-help").style.display="none";
+
+    document.getElementById("mail-client").style.display="none";
+
+}
+function free_help_card(){
+    document.getElementById("getting-started").style.display="none";
+    document.getElementById("what-to-bring").style.display="none";
+    document.getElementById("Institute-account").style.display="none";
+    document.getElementById("get-online").style.display="none";
+    document.getElementById("getting-on-email").style.display="none";
+    document.getElementById("get-help").style.display="none";
+    document.getElementById("free-help").style.display="block";
+    document.getElementById("mail-client").style.display="none";
+}
+function It_service_card(){
+    document.getElementById("It-service").style.display="none";
+    document.getElementById("It-support").style.display="block";
+    document.getElementById("access-email").style.display="none";
+    document.getElementById("password-Reset").style.display="none";
+    document.getElementById("VPN-connection").style.display="none";
+    document.getElementById("new-student-support").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+
+}
+function access_email_card(){
+    document.getElementById("It-service").style.display="none";
+    document.getElementById("It-support").style.display="none";
+    document.getElementById("access-email").style.display="block";
+    document.getElementById("password-Reset").style.display="none";
+    document.getElementById("VPN-connection").style.display="none";
+    document.getElementById("new-student-support").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+}
+function password_reset_card(){
+    document.getElementById("It-service").style.display="none";
+    document.getElementById("It-support").style.display="none";
+    document.getElementById("access-email").style.display="none";
+    document.getElementById("password-Reset").style.display="block";
+    document.getElementById("VPN-connection").style.display="none";
+    document.getElementById("new-student-support").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+    
+
+}
+function vpn_connection_card(){
+    document.getElementById("It-service").style.display="none";
+    document.getElementById("It-support").style.display="none";
+    document.getElementById("access-email").style.display="none";
+    document.getElementById("password-Reset").style.display="none";
+    document.getElementById("VPN-connection").style.display="block";
+    document.getElementById("new-student-support").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+}
+function new_student_card(){
+    document.getElementById("It-service").style.display="none";
+    document.getElementById("It-support").style.display="none";
+    document.getElementById("access-email").style.display="none";
+    document.getElementById("password-Reset").style.display="none";
+    document.getElementById("VPN-connection").style.display="none";
+    document.getElementById("new-student-support").style.display="block";
+    document.getElementById("mail-client").style.display="none";
+
+}
+function menu1(){
+    document.getElementById("getting-started").style.display="block";
+    document.getElementById("what-to-bring").style.display="block";
+    document.getElementById("Institute-account").style.display="block";
+    document.getElementById("get-online").style.display="block";
+    document.getElementById("getting-on-email").style.display="block";
+    document.getElementById("get-help").style.display="block";
+    document.getElementById("free-help").style.display="block";
+
+   
+    
+
+    document.getElementById("It-service").style.display="none";
+    document.getElementById("It-support").style.display="none";
+    document.getElementById("access-email").style.display="none";
+    document.getElementById("password-Reset").style.display="none";
+    document.getElementById("VPN-connection").style.display="none";
+    document.getElementById("new-student-support").style.display="none";
+
+    document.getElementById("mail-client").style.display="none";
+
+}
+function menu2(){
+    document.getElementById("getting-started").style.display="none";
+    document.getElementById("what-to-bring").style.display="none";
+    document.getElementById("Institute-account").style.display="none";
+    document.getElementById("get-online").style.display="none";
+    document.getElementById("getting-on-email").style.display="none";
+    document.getElementById("get-help").style.display="none";
+    document.getElementById("free-help").style.display="none";
+   
+
+    document.getElementById("It-service").style.display="block";
+    document.getElementById("It-support").style.display="block";
+    document.getElementById("access-email").style.display="block";
+    document.getElementById("password-Reset").style.display="block";
+    document.getElementById("VPN-connection").style.display="block";
+    document.getElementById("new-student-support").style.display="block";
+
+    document.getElementById("mail-client").style.display="none";
+
+}
+function menu3(){
+    document.getElementById("getting-started").style.display="none";
+    document.getElementById("what-to-bring").style.display="none";
+    document.getElementById("Institute-account").style.display="none";
+    document.getElementById("get-online").style.display="none";
+    document.getElementById("getting-on-email").style.display="none";
+    document.getElementById("get-help").style.display="none";
+    document.getElementById("free-help").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+
+    document.getElementById("It-service").style.display="none";
+    document.getElementById("It-support").style.display="none";
+    document.getElementById("access-email").style.display="none";
+    document.getElementById("password-Reset").style.display="none";
+    document.getElementById("VPN-connection").style.display="none";
+    document.getElementById("new-student-support").style.display="none";
+
+    document.getElementById("mail-client").style.display="block";
+}
+function menu4(){
+    document.getElementById("getting-started").style.display="none";
+    document.getElementById("what-to-bring").style.display="none";
+    document.getElementById("Institute-account").style.display="none";
+    document.getElementById("get-online").style.display="none";
+    document.getElementById("getting-on-email").style.display="none";
+    document.getElementById("get-help").style.display="none";
+    document.getElementById("free-help").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+
+    document.getElementById("It-service").style.display="none";
+    document.getElementById("It-support").style.display="none";
+    document.getElementById("access-email").style.display="none";
+    document.getElementById("password-Reset").style.display="none";
+    document.getElementById("VPN-connection").style.display="none";
+    document.getElementById("new-student-support").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+    document.getElementById("open-edx").style.display="none";
+    document.getElementById("moodle").style.display="block";
+}
+function menu5(){
+    document.getElementById("getting-started").style.display="none";
+    document.getElementById("what-to-bring").style.display="none";
+    document.getElementById("Institute-account").style.display="none";
+    document.getElementById("get-online").style.display="none";
+    document.getElementById("getting-on-email").style.display="none";
+    document.getElementById("get-help").style.display="none";
+    document.getElementById("free-help").style.display="none";
+   
+
+    document.getElementById("It-service").style.display="none";
+    document.getElementById("It-support").style.display="none";
+    document.getElementById("access-email").style.display="none";
+    document.getElementById("password-Reset").style.display="none";
+    document.getElementById("VPN-connection").style.display="none";
+    document.getElementById("new-student-support").style.display="none";
+    document.getElementById("mail-client").style.display="none";
+    document.getElementById("open-edx").style.display="block";
+    document.getElementById("moodle").style.display="none";
+}
